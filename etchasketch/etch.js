429e8a93e879965ac3b8cfb9344f396ec9ef84b3
Keith <kmroan@gmail.com>
2024-01-16 05:54:26 -0500

    Finished etch-a-sketch project

--- etchasketch/etch.js
+++ etchasketch/etch.js
@@ -1,8 +1,13 @@
 const gridSlider = document.querySelector("#gridSlider");
-const gridCtr = document.querySelector("#grid");
+const gridCtr = document.querySelector("#gridCtr");
+const grid = document.querySelector("#grid");
 const lblgridSlider = document.querySelector("#lblGridSlider");
+const btnReset = document.querySelector("#btnReset");
+const btnRed = document.querySelector("#btnRed");
+const btnRandom = document.querySelector("#btnRandom");
 
-//Set slider lable to curernt value
+let color = "black"
+
 drawGrid(16)
 gridSlider.addEventListener("input", (e) => {
     lblgridSlider.textContent = e.target.value;
@@ -12,20 +17,34 @@ gridSlider.addEventListener("input", (e) => {
 function drawGrid(gridSize) {
 
     // Clear existing grid?
-    if (gridCtr.hasChildNodes()) {
-        while (gridCtr.firstChild) {
-            gridCtr.removeChild(gridCtr.firstChild)
+    if (grid.hasChildNodes()) {
+        while (grid.firstChild) {
+            grid.removeChild(grid.firstChild)
         }
     }
-    
-    for (let i = 0; i < (gridSize); i++) {
+
+    for (let i = 0; i < (gridSize**2); i++) {
         const gridBox = document.createElement('div');
+        gridBox.style.flex = `1 0 calc(100% / ${gridSize})`
         gridBox.className = "gridBox"
-        gridCtr.appendChild(gridBox);
+        grid.appendChild(gridBox);
     }
 
 }
 
-console.log(
-    gridCtr.children.length
-)
+btnReset.addEventListener('click',()=>{drawGrid(gridSlider.value)})
+btnRed.addEventListener('click',() => { color = "red"})
+btnBlack.addEventListener('click',() => { color = "black"})
+btnRandom.addEventListener('click',() => { 
+    color = "random"
+    //color = `rgb(${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)})`
+})
+grid.addEventListener("mouseover",(e) => {
+    if (color === "random") {
+        color = `rgb(${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)})`
+        console.log(color)
+    } else {
+        e.target.style.background = color;
+    }
+    
+})
